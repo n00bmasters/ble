7858f8f105c8830a2e9b67b9c8b1cbc075d2ca94
George <vasilevskygeorge@gmail.com>
2025-10-01 15:42:28 +0700

    it works

--- firmware/ble-catch/main.py
+++ firmware/ble-catch/main.py
@@ -4,38 +4,37 @@ import time
 import ubinascii
 import json
 
+ble = bluetooth.BLE()
+ble.active(True)
 
-print("BLE scanner started...")
 
-while True:
-    try:
-        print("\n--- New Scan Cycle ---")
-
-        ble = bluetooth.BLE()
-        ble.active(True)
+def scan_callback(event, data):
+    # event 5 - это _IRQ_SCAN_RESULT
+    if event == 5:
+        addr_type, addr_bytes, adv_type, rssi, adv_data_bytes = data
         
-        found_devices = ble.gap_scan(8000, 130000, 110000)
-        
-        if not found_devices:
-            print("No devices found.")
-        else:
-            print(f"Found {len(found_devices)} devices.")
-            output_data = []
-            for device in found_devices:
-                addr_type, addr_bytes, adv_type, rssi, adv_data_bytes = device
-                device_info = {
-                    "mac": ubinascii.hexlify(addr_bytes, ':').decode().upper(),
-                    "rssi": rssi,
-                    "raw_adv_data": ubinascii.hexlify(adv_data_bytes).decode(),
-                    "addr_type": addr_type,
-                    "adv_type": adv_type
-                }
-                output_data.append(device_info)
-            
-            print(json.dumps(output_data))
+        device_info = {
+            "mac": ubinascii.hexlify(addr_bytes, ':').decode().upper(),
+            "rssi": rssi,
+            "raw_adv_data": ubinascii.hexlify(adv_data_bytes).decode(),
+            "addr_type": addr_type,
+            "adv_type": adv_type
+        }
+        print(json.dumps(device_info))
 
-    except OSError as e:
+ble.irq(scan_callback)
 
-        print(f"An unexpected error occurred: {e}")
+print("Starting ACTIVE ASYNC scanner...")
 
-    time.sleep(2)
+# ---  СКАНИРОВАНИЕ ПО ДОКАМ ---
+# scan(0, ...) -> сканировать бесконечно
+# active=True -> включаем АКТИВНОЕ сканирование
+ble.gap_scan(0, 150000, 130000, True)
+
+
+try:
+    while True:
+        time.sleep_ms(100)
+except KeyboardInterrupt:
+    ble.gap_scan(None)
+    print("Scanner stopped.")
