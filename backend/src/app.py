aab607bfa9cd1afc141c73a4166e6862e4326104
Qangan <danilfulmanf@gmail.com>
2025-10-02 13:14:55 +0700

    misha fixi 6

--- backend/src/app.py
+++ backend/src/app.py
@@ -2,11 +2,23 @@ import paho.mqtt.client as mqtt
 import numpy as np
 from math_mod import DistanceCalc, Kalman
 import json
+import matplotlib.pyplot as plt
 
 rssi = []
 f_rssi = []
 calc = None
+cur_pos = (0,0)
 #points = {}
+
+plt.ion()  # Turn on interactive mode
+fig, ax = plt.subplots()
+x_data = []
+y_data = []
+line, = ax.plot(x_data, y_data, 'ro-')  # Red circles with lines
+ax.set_xlabel('X Position')
+ax.set_ylabel('Y Position')
+ax.set_title('Real-time Position Tracking')
+ax.grid(True)
 
 def on_connect(client, userdata, flags, reason_code, properties):
     global rssi
@@ -20,33 +32,59 @@ def on_connect(client, userdata, flags, reason_code, properties):
         for i in fp:
             st, x, y = i.split(';')
             points[int(st[-1])] = tuple(map(float, (x, y)))
-        print(points)
-        calc = DistanceCalc(points, [-50 for i in range(len(points))])
+        #print(points)
+        calc = DistanceCalc(points, [-43, -42, -41, -44, -43, -40, -41, -41])
         rssi = [Kalman() for i in range(len(points))]
         f_rssi = [0 for i in range(len(points))]
         #print(rssi)
 
+        beacon_x = [point[0] for point in points.values()]
+        beacon_y = [point[1] for point in points.values()]
+        beacon_plot = ax.plot(beacon_x, beacon_y, 'b^', markersize=10, label='Beacons')[0]
+        
+        # Initialize current position plot
+        global position_plot
+        position_plot = ax.plot([cur_pos[0]], [cur_pos[1]], 'ro', markersize=8, label='Current Position')[0]
+        
+        ax.set_xlabel('X Position')
+        ax.set_ylabel('Y Position')
+        ax.set_title('Real-time Position Tracking')
+        ax.grid(True)
+        ax.legend()
+
+        # Set reasonable axis limits based on beacon positions
+        if beacon_x and beacon_y:
+            margin = 2
+            ax.set_xlim(min(beacon_x) - margin, max(beacon_x) + margin)
+            ax.set_ylim(min(beacon_y) - margin, max(beacon_y) + margin)
 def on_message(client, userdata, msg):
     global rssi
     global f_rssi
-    nodes_available = []
+    global cur_pos
     rssis = []
     old = 0
     meow = msg.payload.decode("utf-8")
     js = json.loads(meow)["pack"]
-    print(js)
-    print(meow)
+    #print(js)
+    #print(meow)
     for i in js:
-        print(i)
+        #print(i)
         name = i['name']
-        nodes_available.append(int(name[name.find('_')+1:]))
-        #print(int(name[name.find('_')+1:]))
-        rssis.append(rssi[int(name[name.find('_')+1:]) - 1].apply_kalman_filter(i['rssi']))
+        rssis.append([int(name[name.find('_')+1:]), float(i["rssi"])])
+        #rssis.append([int(name[name.find('_')+1:]), rssi[int(name[name.find('_')+1:]) - 1].apply_kalman_filter(i['rssi'])])
 
-    if len(nodes_available) < 3:
+    if len(rssis) < 3:
         print('less than 3 available nodes, fix is not obtained')
     else:
-        print(calc.get_pos(nodes_available, rssis))
+        tmp_pos = calc.get_pos(rssis)
+        cur_pos = cur_pos if abs(cur_pos[0] - tmp_pos[0]) > 7 or abs(cur_pos[1] - tmp_pos[1]) > 7 else tmp_pos
+        # Update only the current position dot
+        position_plot.set_data([cur_pos[0]], [cur_pos[1]])
+        
+        # Refresh the plot
+        fig.canvas.draw()
+        fig.canvas.flush_events()      
+        print(cur_pos)
 
 
     
@@ -66,4 +104,10 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect("127.0.0.1", 1883, 60)
-client.loop_forever()
+try:
+    client.loop_forever()
+except KeyboardInterrupt:
+    print("Stopping...")
+finally:
+    plt.ioff()
+    plt.show()
--- backend/src/math_mod.py
+++ backend/src/math_mod.py
@@ -2,7 +2,7 @@ import scipy
 import numpy as np
 from filterpy.kalman import KalmanFilter
 
-UNCERTAINTY = 25
+UNCERTAINTY = 20
 
 class Kalman:
     def __init__(self):
@@ -26,14 +26,17 @@ class DistanceCalc:
         self.trans = trans
         self.def_power = def_power #СУКИ КАЛИБРУЙТЕ ОТ ОДНОЙ НОДЫ, А НЕ ОТ ВСЕХ
         self.scale = 32 
-        self.ple = 3
+        self.ple = 2.7
 
-    def get_pos(self, available_nodes: list[int], rssi: list[float]):
+    def get_pos(self, rssis: list[list[int, float]]):
         d = dict()
         #print(available_nodes, rssi)
-        for i in range(len(available_nodes)):
-            d[available_nodes[i]] = self.get_dist(rssi[i], available_nodes[i])
-        est_x, est_y = self.trilaterate(d, available_nodes)
+        rssis.sort(key=lambda x: x[1], reverse=True)
+        av = []
+        for i in rssis[:3]:
+            d[i[0]] = self.get_dist(i[1], i[0])
+            av.append(i[0])
+        est_x, est_y = self.trilaterate(d, av)
         return est_x, est_y
     
     def trilaterate(self, d, available_nodes):
