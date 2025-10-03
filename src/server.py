76d7c7739bbb720f24c19513294a6e058f3874d5
M-K-S-P <miha.kokh@gmail.com>
2025-10-03 12:00:34 +0700

    server done

--- src/server.py
+++ src/server.py
@@ -172,4 +172,4 @@ async def get_settings():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run('server:app', host='127.0.0.1', port=8080, reload=True)
+    uvicorn.run('server:app', host='0.0.0.0', port=8080, reload=True)
